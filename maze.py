38565fcab9b16416076b1a9adfddf8b10acdf982
Wilka Carvalho <wcarvalho92@gmail.com>
2024-06-24 20:42:41 -0400

    housemaze obs is now categorical integers, not one-hots

--- maze.py
+++ maze.py
@@ -249,6 +249,15 @@ class HouseMaze:
         self.action_spec = action_spec
         self.use_done = use_done
 
+    def total_categories(self, params: EnvParams):
+        grid = params.map_init.grid
+        H, W = grid.shape[-3:-1]
+        num_object_categories = self.num_categories
+        num_directions = len(DIR_TO_VEC)
+        num_spatial_positions = H * W
+        num_actions = self.num_actions(params)
+        return num_object_categories + num_directions + num_spatial_positions + num_actions
+
     def action_enum(self):
         if self.action_spec == 'keyboard':
             return KeyboardActions
@@ -270,34 +279,46 @@ class HouseMaze:
         return one_hot
 
     def make_observation(
-            self,
-            state: EnvState,
-            prev_action: jax.Array):
-        """This converts all inputs into binary vectors. this faciitates processing with a neural network."""
+        self,
+        state: EnvState,
+        prev_action: jax.Array):
+        """This converts all inputs into categoricals.
 
+        Categories are [objects, directions, spatial positions, actions]
+        """
         grid = state.grid
         agent_pos = state.agent_pos
         agent_dir = state.agent_dir
 
-        # direction
-        direction = jnp.zeros((len(DIR_TO_VEC)))
-        direction = direction.at[agent_dir].set(1)
+        # Compute the total number of categories
+        num_object_categories = self.num_categories
+        num_directions = len(DIR_TO_VEC)
+        H, W = grid.shape[-3:-1]
+        num_spatial_positions = H*W
 
-        make_binary_vector_ = lambda g: make_binary_vector(g, num_categories=self.num_categories)
+        #num_actions = self.num_actions()
+        #total_categories = self.total_categories
 
-        make_binary_vector_grid = jax.vmap(jax.vmap(make_binary_vector_))
+        # Convert direction to the right category integer. after [objects]
+        direction_category = num_object_categories + agent_dir
+
+        # Convert position to the right category integer. after [objects, directions]
+        position_category = num_object_categories + num_directions + agent_pos[0] * H + agent_pos[1]
+
+        # Convert prev_action to the right category integer. after [objects, directions, spatial positions]
+        prev_action_category = num_object_categories + num_directions + num_spatial_positions + prev_action
 
         observation = Observation(
-            image=make_binary_vector_grid(grid),
+            image=jnp.squeeze(state.grid).astype(jnp.int32),
             state_features=state.task_state.features.astype(jnp.float32),
             task_w=state.task_w.astype(jnp.float32),
-            direction=direction,
-            position=position_to_two_hot(agent_pos, grid.shape[:2]),
-            prev_action=self.action_onehot(prev_action),
+            direction=jnp.array(direction_category, dtype=jnp.int32),
+            position=jnp.array(position_category, dtype=jnp.int32),
+            prev_action=jnp.array(prev_action_category, dtype=jnp.int32),
         )
-        # just to be safe?
-        observation = jax.tree_map(lambda x: jax.lax.stop_gradient(x), observation)
 
+        # Just to be safe?
+        observation = jax.tree_map(lambda x: jax.lax.stop_gradient(x), observation)
         return observation
 
     def reset(self, rng: jax.Array, params: EnvParams) -> TimeStep:
