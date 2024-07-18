99035c89313050fd269e4c3a0da84f4808903bd7
Wilka Carvalho <wcarvalho92@gmail.com>
2024-07-18 10:41:25 -0400

    changing names

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="JaxHouseMaze",
+    name="jaxhousemaze",
     version="0.0.1",
     packages=find_packages(),
     install_requires=[
